d63b820b9b49f9178553630196292e9764ea9a72
ShanSanear <medan1993@gmail.com>
2020-05-29 11:22:05 +0200

    Adding changes

--- main.cpp
+++ main.cpp
@@ -6,19 +6,6 @@
 #include <fstream>
 #include <cstdlib>
 
-double A, B, C;
-
-struct CalculateParameters
-{
-    double A;
-    double B;
-    double C;
-    int N_quadratic;
-    double start_point;
-    double end_point;
-    int N_Pi;
-};
-
 //using matrix = std::vector<std::vector<double>>;
 
 std::vector<std::vector<double> > load_csv(const std::string input_csv_file) {
@@ -59,12 +46,9 @@ int get_int_from_stdin(const char *message)
 int main()
 {
     int standardPrecision = 6;
-    int piPrecision = 20;
-    int N_quadratic, N_Pi;
     const int struct_size = 7;
-    double sum_quadratic, sum_pi, dx, dt, start_point, end_point, result_quadratic, result_pi;
     double startTime, endTime, parallelTimeTaken, timeSingle;
-    plog::RollingFileAppender<plog::CsvFormatter> fileAppender("Datalogger.txt", 1048576, 5);
+    plog::RollingFileAppender<plog::TxtFormatter> fileAppender("Datalogger.txt", 1048576, 5);
     plog::ConsoleAppender<plog::TxtFormatter> consoleAppender;
     plog::init(plog::info, &fileAppender).addAppender(&consoleAppender);
     int node, numOfNodes;
@@ -72,29 +56,17 @@ int main()
     MPI_Comm_size(MPI_COMM_WORLD, &numOfNodes);
     MPI_Comm_rank(MPI_COMM_WORLD, &node);
     // Change datatype
-    int lengths[struct_size] = {1, 1, 1, 1, 1, 1, 1};
-    MPI_Datatype types[struct_size] = {MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_INTEGER, MPI_DOUBLE, MPI_DOUBLE, MPI_INTEGER};
-    MPI_Aint displacements[struct_size] = {
-        offsetof(CalculateParameters, A),
-        offsetof(CalculateParameters, B),
-        offsetof(CalculateParameters, C),
-        offsetof(CalculateParameters, N_quadratic),
-        offsetof(CalculateParameters, start_point),
-        offsetof(CalculateParameters, end_point),
-        offsetof(CalculateParameters, N_Pi)
-        };
     
-    MPI_Datatype mpiCalculateParametersDatatype;
-    MPI_Type_create_struct(struct_size, lengths, displacements, types, &mpiCalculateParametersDatatype);
-    MPI_Type_commit(&mpiCalculateParametersDatatype);
     // End of creating datatype
-
+    double C[3][3] = { 0 };
     std::stringstream resultStream;
     resultStream << std::fixed << std::setprecision(standardPrecision);
     if (node == 0)
     {
         PLOG_INFO << "Getting input values";
         //get input parameters
+        double A[3][3] = { {1,2,3}, {4,5,6}, {7,8,9} };
+        double B[3][3] = { {1,1,1}, {2,2,2}, {3,3,3} };
         
 
         startTime = MPI_Wtime();
@@ -103,65 +75,81 @@ int main()
         endTime = MPI_Wtime();
         timeSingle = endTime - startTime;
         // TODO rename this string
-        resultStream << "Single node integral result: " << static_cast<double>(sum_quadratic);
         PLOG_INFO << resultStream.str();
         PLOG_INFO << "Single node time: " << timeSingle << " second(s)";
         resultStream << std::setprecision(standardPrecision);
-        struct CalculateParameters calculateStruct;
         // prepare structure
         startTime = MPI_Wtime();
-        calculateStruct.A = A;
-        calculateStruct.B = B;
-        calculateStruct.C = C;
-        calculateStruct.N_quadratic = N_quadratic;
-        calculateStruct.start_point = start_point;
-        calculateStruct.end_point = end_point;
-        calculateStruct.N_Pi = N_Pi;
         PLOG_INFO << "Sending data to other nodes";
         for (int i = 1; i < numOfNodes; i++)
         {
-            MPI_Send(&calculateStruct, 1, mpiCalculateParametersDatatype, i, 0, MPI_COMM_WORLD);
+            //MPI_Send(&calculateStruct, 1, mpiCalculateParametersDatatype, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&A[i][0], 3, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
             
         }
+        MPI_Bcast(&B[0][0], 9, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         // Process node 0 part of parallel processing
+        //for (int i = 1; i < numOfNodes; i++) {
+        //    MPI_Recv(&C[i][0], 3, MPI_DOUBLE,i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        //}
     }
     else
     {
-        struct CalculateParameters calcStruct;
-        MPI_Recv(&calcStruct, 1, mpiCalculateParametersDatatype, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        //struct CalculateParameters calcStruct;
+        //MPI_Recv(&calcStruct, 1, mpiCalculateParametersDatatype, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         
         // Processing parallel part for other nodes
+
+        double B[3][3] = { 0};
+        double LocalA[3] = { 0 };
+        double LocalC[3] = { 0 };
+        B[1][1] = 1.0;
+
         PLOG_INFO << "Calculating integral, node: " << node;
         
-        A = calcStruct.A;
-        B = calcStruct.B;
-        C = calcStruct.C;
-        N_quadratic = calcStruct.N_quadratic;
-        N_Pi = calcStruct.N_Pi;
-        start_point = calcStruct.start_point;
-        end_point = calcStruct.end_point;
+        MPI_Recv(LocalA, 3, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Bcast(&B[0][0], 9, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        //matrix output_matrix(matrix_a.size(), std::vector<double>(matrix_b.at(0).size()));
+        // int output_rows = output_matrix.size();
+        // int output_columns = output_matrix.at(0).size();
+        // int inner_size = matrix_b.size();
+        // int output_rows = 1;
+        int output_columns = 3;
+        int inner_size = 3;
+        printf("Multiplying matrixes using sequential method\n");
+        for (int col = 0; col < output_columns; col++) {
+            for (int inner = 0; inner < inner_size; inner++) {
+                PLOG_INFO << "Column: " << col << " inner: " << inner;
+                LocalC[col] += LocalA[inner] * B[inner][col];
+            }
+        }
+        
+        //MPI_Send(LocalC, 3, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     }
     PLOG_INFO << "Finished processing, node: " << node;
 
-    MPI_Reduce(&sum_quadratic, &result_quadratic, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&sum_pi, &result_pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    
+  
     MPI_Barrier(MPI_COMM_WORLD);
     if (node == 0)
     {
         //Postprocessing
-        endTime = MPI_Wtime();
-        parallelTimeTaken = endTime - startTime;
-        resultStream.str(std::string());
-        resultStream << "Parallized result: " << static_cast<double>(result_quadratic);
-        PLOG_INFO << resultStream.str();
-        resultStream.str(std::string());
-        resultStream << std::setprecision(piPrecision);
-        resultStream << "Parallized Pi result: " << static_cast<double>(result_pi);
-        PLOG_INFO << resultStream.str();
-        PLOG_INFO << "Parallized time: " << parallelTimeTaken << " second(s)";
+        // endTime = MPI_Wtime();
+        // parallelTimeTaken = endTime - startTime;
+        // resultStream.str(std::string());
+        // resultStream << "Parallized result: " << static_cast<double>(result_quadratic);
+        // PLOG_INFO << resultStream.str();
+        // resultStream.str(std::string());
+        // resultStream << std::setprecision(piPrecision);
+        // resultStream << "Parallized Pi result: " << static_cast<double>(result_pi);
+        // PLOG_INFO << resultStream.str();
+        // PLOG_INFO << "Parallized time: " << parallelTimeTaken << " second(s)";
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                printf("%f, ", C[i][j]);
+            }
+            printf("\n");
+        }
     }
-    MPI_Type_free(&mpiCalculateParametersDatatype);
     MPI_Finalize();
     return 0;
 }
